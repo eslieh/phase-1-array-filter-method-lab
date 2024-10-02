16c7a8a3971355db41eb22a56a979e490c3d2786
mashfied <victoreslieh@gmail.com>
2024-10-02 12:13:43 +0300

    passed test

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function findMatching(drivers, driver){
+    const matching = drivers.filter((name) => name.toLowerCase() === driver.toLowerCase());
+    return matching;
+}
+
+function fuzzyMatch(drivers, driver){
+    const matching = drivers.filter((name) => name.slice(0,2).toLowerCase() === driver.toLowerCase())
+    return matching;
+}
+function matchName(drivers, driver){
+    const matching = drivers.filter(({name}) => name.toLowerCase() === driver.toLowerCase());
+    return matching;
+}
